c331ccbbdc17792ae245a2173e0fd8d5f32516b9
Alexandre Brun <alexandre.brun@xcg-consulting.fr>
2013-12-18 14:01:32 +0100

    Refactoring

--- tank/main.py
+++ tank/main.py
@@ -1,20 +1,20 @@
 from tank import Tank
 
+
 def main():
-    tankalex = Tank(5, 5)
+    tankalex = Tank(1, 1)
+    #battlefield = Battlefield()
+    #battefield.add_elements(tankalex)
 
     tankalex.move_forward()
     tankalex.move_forward()
     tankalex.turnleft()
     tankalex.fire()
-    tankalex.hide()
     tankalex.move_forward()
     tankalex.move_forward()
     tankalex.fire()
     tankalex.fire()
     tankalex.reload()
-    tankalex.hide()
-
     tankalex.turnleft()
     tankalex.turnleft()
     tankalex.move_forward()
--- tank/tank.py
+++ tank/tank.py
@@ -3,38 +3,45 @@
 # possible directions
 enum_directions = {
     'north': 0,
-    'east' : 1,
+    'east': 1,
     'south': 2,
     'west': 3,
 }
 
 
-# This is a class to declare the first class Tank with specifies.
-# The Tank fight with ammo which decrease each time the player fires
-# and directions which are shown in a grid.   
-class Tank(object):
+class Element(object):
+    """This is a class that designates
+    an object in the battlefield
+    """
+
     def __init__(self, x, y):
-        self.ammo = 50
         self.x = x
         self.y = y
+
+
+class Vehicule(Element):
+    """This is a class to manage directions
+    in the battlefield
+    """
+
+    def __init__(self, x, y):
+        super(Vehicule, self).__init__(x, y)
         self.direction = enum_directions['north']
 
-
-# This functions are for movements. A Tank can move in 4 diffrents directions.
     def move_forward(self):
         print('Your tank moves forward')
 
         if self.direction == enum_directions['north']:
-            self.y += 1 
+            self.y += 1
 
         elif self.direction == enum_directions['east']:
-            self.x += 1 
+            self.x += 1
 
         elif self.direction == enum_directions['south']:
             self.y -= 1
 
         elif self.direction == enum_directions['west']:
-            self.x -= 1    
+            self.x -= 1
 
         print('Your tank is now at (%d, %d)' % (self.x, self.y))
 
@@ -44,7 +51,7 @@ class Tank(object):
             self.direction = 0
         else:
             self.direction += 1
-        
+
     def turnleft(self):
         print('Your tank turns 90° left')
         if self.direction == 0:
@@ -52,7 +59,16 @@ class Tank(object):
         else:
             self.direction -= 1
 
-    #This function manages the arming
+
+class Tank(Vehicule):
+    """This is a class to manage the weapons
+    """
+
+    def __init__(self, x, y):
+        super(Tank, self).__init__(x, y)
+        self.ammo = 50
+        print ("Creation d'une instance de Tank")
+
     def fire(self):
         if not self.ammo <= 0:
             print('Your tank fires')
@@ -61,14 +77,15 @@ class Tank(object):
         else:
             print('You cannot fire when ammo is empty')
 
-    #This function handles reloading ammos
     def reload(self):
+        """This function handles reloading ammos
+        """
         print('Your tank reloads')
         self.ammo = 50
- 
-    #This function handles the hiding on the battlefield
-    #How can I declare reserved hiding areas?
-    def hide(self):
-        print ('Your Tank is now hidden at (%d, %d)' %(self.x, self.y))
-        
 
+
+#class Battlefield(object):
+# def __init__(self):
+#      self.elements = []
+#
+# def add_elements(self, elements):
